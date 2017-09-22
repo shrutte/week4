4e00e94d7c319cc0f82fbf80c40b05acbbd4ca2a
shrutte <coolshrutte@gmail.com>
2017-09-22 20:45:25 +0530

    first commit

---
+++ app/src/main/java/com/example/user/week4/Week4.java
@@ -0,0 +1,46 @@
+package com.example.user.week4;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Week4 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week4);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.alertopen) void open(View view){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure, You wanted to make decision");
+        alertDialogBuilder.setPositiveButton("yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(Week4.this,"You clicked yes button",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
+
---
+++ app/src/test/java/com/example/user/week4/Week4Test.java
@@ -0,0 +1,10 @@
+package com.example.user.week4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by USER on 8/1/2017.
+ */
+public class Week4Test {
+
+}
